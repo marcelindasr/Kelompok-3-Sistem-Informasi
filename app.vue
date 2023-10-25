7d70a233f9b6ad4247db5e0490948838c9ee2630
Muthia Anashya Salam <118595769+muthiasalam@users.noreply.github.com>
2023-10-25 19:36:00 +0800

    Update DharmaWanita

--- app.vue
+++ app.vue
@@ -232,33 +232,35 @@
   </div>
 </div>
 
-  <div class="dharmawarnita mt-30 flex mb-30 bg-abu">
-    <div class="foto w-2/5 h-1/4">
-      <div class="foto-dharma w-full h-full">
-        <img src="./assets/sambutan/Group 205200.png" class="foto">
-      </div>
+  <div class = "dharmawarnita mt-30 flex mb-35  flex-col md:flex-row bg-abu">
+  <div class="foto md:w-2/5 w-full h-auto relative overflow-hidden">
+    
+    <div class="foto-dharma w-full h-full relative">
+      <img src="./assets/sambutan/bg.png" class="foto object-cover w-full h-full">
     </div>
-    <div class="dharma-kanan flex flex-col justify-center w-3/5 pr-20">
-      <div class="dharma-kanan-top mb-0.5 pl-20 pr-10">
-        <p id="title" class="text-19 font-bold text-merahdesain">DHARMA WANITA</p>
-      </div>
-      <div class="dharma-kanan-top mb-0.5 pl-20 pr-10">
-        <p id="title" class="text-19 font-bold text-merahdesain">PERSATUAN FAKULTAS TEKNIK</p>
-      </div>
-      <hr class="my-4 w-3/5 border-1 border-abunama mb-7 mt-1.5 ml-20 mr-10">
-      <div class="dharma-kanan-bottom text-justify pl-20 pr-10">
-        <span id="desc" class="text-12 text-tulisan text-abusambutan">Dharma Wanita Persatuan Fakultas Teknik Unhas yang
-          diketuai oleh Ny. Muralia Isran Ramli, aktif melakukan berbagai macam kegiatan seperti amaliah ramadhan, arisan
-          di kapal, kegiatan BKMT dan DPW Unhas.</span>
-      </div>
-      <div class="tombol ml-auto pr-10">
-        <button class="bg-merahdesain text-white text-14 py-2 px-4 rounded-none">
-          Selengkapnya
-        </button>
-      </div>
+    
+    <div class="foto-tambahan absolute inset-0 flex items-center justify-center">
+      <img src="./assets/sambutan/top.png" class="w-2/3">
     </div>
-
   </div>
+<div class="dharma-kanan flex flex-col justify-center items-center md:items-start w-full md:w-3/5 md:pr-20 h-full pl-10 pr-10 pb-10">
+<div class="dharma-kanan-top mb-0.5 pl-10 pr-10 pt-12">
+  <p id="title" class="text-19 font-bold text-merahdesain">DHARMA WANITA</p>
+</div>
+<div class="dharma-kanan-top mb-0.5 pl-10 pr-10">
+  <p id="title" class="text-19 font-bold text-merahdesain">PERSATUAN FAKULTAS TEKNIK</p>
+</div>
+<hr class="my-4 w-3/5 border-1 border-abunama mb-7 mt-1.5 ml-10 mr-10">
+<div class="dharma-kanan-bottom text-justify pl-10 pr-10">
+  <span id="desc" class="text-12 text-tulisan text-abusambutan">Dharma Wanita Persatuan Fakultas Teknik Unhas yang diketuai oleh Ny. Muralia Isran Ramli, aktif melakukan berbagai macam kegiatan seperti amaliah ramadhan, arisan di kapal, kegiatan BKMT dan DPW Unhas.</span>
+</div>
+<div class="tombol ml-auto mt-10 pr-10">
+  <button class="bg-merahdesain text-white text-14 py-2 px-4 rounded-none hover:bg-red-400" onclick="window.location.href = 'https://eng.unhas.ac.id/fakultas/dharmawanita';">
+    Selengkapnya
+  </button>
+</div>
+</div>
+</div> 
 
   <div class="Berita mb-5 ml-4 mr-8 flex flex-wrap items-start justify-start pb-4">
     <div class="judul text-2xl mb-4 mt-8 font-semibold text-black">BERITA</div>
