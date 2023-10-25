678c3e2fbf6e1f1a56c610afccec274a63473547
Muthia Anashya Salam <118595769+muthiasalam@users.noreply.github.com>
2023-10-25 19:15:58 +0800

    update sambutan

--- app.vue
+++ app.vue
@@ -136,32 +136,29 @@
     </section>
   </div>
 
-  <div class="sambutan-container flex bg-abu mt-12 mb-12 justify-center items-center">
-    <div class="sambutan-kiri flex justify-center items-center w-2/5 h-4/5 pl-10 pr-20">
-      <div class="w-1/5"></div>
-      <div class="dekan-kontainer w-4/5 pt-10 pb-10">
-        <img src="./assets/sambutan/Group 195.png" class="foto">
-      </div>
-    </div>
-    <div class="sambutan-kanan flex flex-col justify-center items-start w-3/5 h-full pt-10 pb-10 pr-20 pl-10">
-      <div class="sambutan-kanan-top mb-0.5">
-        <p id="title" class="text-19 font-bold text-merahdesain">DEKAN FAKULTAS TEKNIK</p>
-      </div>
-      <div class="sambutan-kanan-mid mb-0.5">
-        <p id="nama" class="text-12 text-abunama font-medium">Prof. Dr. Eng. Ir. Muhammad Isran Ramli, S.T., M.T., IPM.,
-          ASEAN Eng</p>
-      </div>
-      <hr class="my-4 w-3/4 border-1 border-abunama mb-10 mt-1.5">
-      <div class="sambutan-kanan-bottom text-justify pr-10">
-        <span id="quote" class="text-12 text-tulisan text-abusambutan">Kami dari semua civitas akademika Fakultas Teknik
-          dengan semua dukungan komponen perguruan tinggi yang ada akan terus meningkatkan pencapaian menuju terwujudnya
-          visi dan misi yang telah dicanangkan dalam rencana strategis fakultas. Semoga informasi yang diberikan lewat
-          layanan website ini dapat memberikan gambaran situasi dan kondisi Fakultas Teknik kepada semua lapisan
-          masyarakat.</span>
-      </div>
+  <div class="sambutan-container flex flex-col md:flex-row bg-abu mt-12 mb-12 justify-center items-center">
+  <!-- Sambutan Kiri (Kiri di Atas untuk Tablet dan Mobile) -->
+  <div class="sambutan-kiri flex justify-center items-center w-full md:w-2/5 h-auto md:h-4/5 pl-30 pr-30 md:pr-10 md:pl-10 md:order-first">
+    <div class="dekan-kontainer w-4/5 pt-10 pb-10">
+      <!-- Gambar dengan lebar responsif -->
+      <img src="./assets/sambutan/Group 195.png" class="foto w-1/2 sm:w-1/3 md:w-full mx-auto">
     </div>
   </div>
 
+  <!-- Sambutan Kanan (Kanan di Bawah untuk Tablet dan Mobile) -->
+  <div class="sambutan-kanan flex flex-col justify-center items-center md:items-start w-full md:w-3/5 md:pr-20 h-full pl-10 pr-10 pb-10">
+    <div class="sambutan-kanan-top mb-0.5 md:mt-10">
+      <p id="title" class="text-19 font-bold text-merahdesain text-center md:text-left">DEKAN FAKULTAS TEKNIK</p>
+    </div>
+    <div class="sambutan-kanan-mid mb-0.5 pr-10 pl-10 md:pr-0 md:pl-0">
+      <p id="nama" class="text-12 text-abunama font-medium text-center md:text-left">Prof. Dr. Eng. Ir. Muhammad Isran Ramli, S.T., M.T., IPM., ASEAN Eng</p>
+    </div>
+    <hr class="my-4 w-3/4 border-1 border-abunama mb-10 mt-1.5 pr-10 pl-10 md:pr-0 md:pl-0">
+    <div class="sambutan-kanan-bottom text-justify pr-10 pl-10 md:pr-0 md:pl-0">
+      <span id="quote" class="text-12 text-tulisan text-abusambutan">Kami dari semua civitas akademika Fakultas Teknik dengan semua dukungan komponen perguruan tinggi yang ada akan terus meningkatkan pencapaian menuju terwujudnya visi dan misi yang telah dicanangkan dalam rencana strategis fakultas. Semoga informasi yang diberikan lewat layanan website ini dapat memberikan gambaran situasi dan kondisi Fakultas Teknik kepada semua lapisan masyarakat.</span>
+    </div>
+  </div>
+</div>
 
 
 <div id="app" class="w-full mx-auto px-4 md:px-8 py-12 transition-all duration-500 ease-linear">
