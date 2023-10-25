5a0c714944e489f69f00376caeead8e10b6172df
Ardriaauliya <143046470+Ardriaauliya@users.noreply.github.com>
2023-10-25 16:48:14 +0800

    Update app.vue

--- app.vue
+++ app.vue
@@ -164,64 +164,76 @@
 
 
 
-  <div id="app" class="max-w-screen-xl mx-auto px-4 md:px-8 py-12 transition-all duration-500 ease-linear">
-    <div class="judul text-black text-2xl font-semibold mb-10">GALERI</div>
-    <div class="relative">
-      <div
-        class="slides-container h-72 flex snap-x snap-mandatory overflow-hidden overflow-x-auto space-x-2 rounded scroll-smooth before:w-[45vw] before:shrink-0 after:w-[45vw] after:shrink-0 md:before:w-0 md:after:w-0">
-        <div class="slideim aspect-[4/3] h-full flex-shrink-0 snap-center rounded overflow-hidden relative">
-          <img class="w-full h-full object-cover" src="./assets/galeri/foto1.jpg" alt="mountain_image">
-          <div class="absolute top-0 left-0 w-full h-full opacity-50 hover:bg-black"></div>
+<div id="app" class="w-full mx-auto px-4 md:px-8 py-12 transition-all duration-500 ease-linear">
+  <div class="judul text-black text-2xl font-semibold">GALERI </div>
+  <div class="relative">
+    <div class="slides-container h-32 md:h-72 flex snap-x snap-mandatory overflow-hidden overflow-x-auto space-x-2 rounded scroll-smooth ">
+      <div class="b relative group">
+        <div class="slideim aspect-[4/3] h-full flex-shrink-0 snap-center rounded overflow-hidden transition-transform transform-gpu">
+          <img class="w-full h-full object-cover" src="./assets/galeri/foto1.jpeg" alt="Transformasi digital manajemen persuratan(mBerkas)">
         </div>
-        <div class="slideim aspect-[4/3] h-full flex-shrink-0 snap-center rounded overflow-hidden relative">
-          <img class="w-full h-full object-cover" src="./assets/galeri/foto2.jpg" alt="mountain_image">
-        </div>
-        <div class="slideim aspect-[4/3] h-full flex-shrink-0 snap-center rounded overflow-hidden">
-          <img class="w-full h-full object-cover" src="./assets/galeri/foto3.jpg" alt="mountain_image">
-        </div>
-        <div class="slideim aspect-[4/3] h-full flex-shrink-0 snap-center rounded overflow-hidden">
-          <img class="w-full h-full object-cover" src="./assets/galeri/foto4.jpg" alt="mountain_image">
-        </div>
-        <div class="slideim aspect-[4/3] h-full flex-shrink-0 snap-center rounded overflow-hidden">
-          <img class="w-full h-full object-cover" src="./assets/galeri/foto5.jpg" alt="mountain_image">
-        </div>
-        <div class="slideim aspect-[4/3] h-full flex-shrink-0 snap-center rounded overflow-hidden">
-          <img class="w-full h-full object-cover" src="./assets/galeri/foto6.jpg" alt="mountain_image">
-        </div>
-        <div class="slideim aspect-[4/3] h-full flex-shrink-0 snap-center rounded overflow-hidden">
-          <img class="w-full h-full object-cover" src="./assets/galeri/foto7.jpg" alt="mountain_image">
-        </div>
-        <div class="slideim aspect-[4/3] h-full flex-shrink-0 snap-center rounded overflow-hidden">
-          <img class="w-full h-full object-cover" src="./assets/galeri/foto1.jpg" alt="mountain_image">
-        </div>
-        <div class="slideim aspect-[4/3] h-full flex-shrink-0 snap-center rounded overflow-hidden">
-          <img class="w-full h-full object-cover" src="./assets/galeri/foto2.jpg" alt="mountain_image">
-        </div>
-        <div class="slideim aspect-[4/3] h-full flex-shrink-0 snap-center rounded overflow-hidden">
-          <img class="w-full h-full object-cover" src="./assets/galeri/foto1.jpg" alt="mountain_image">
-        </div>
-
+        <div class="absolute inset-0 flex items-center justify-center [text-shadow:0_1px_0_rgb(0_0_0/_100%)] w-full bg-black opacity-0 group-hover:opacity-50"></div>
+        <div class="absolute inset-0 flex items-center justify-center [text-shadow:0_1px_0_rgb(0_0_0/_100%)] text-white w-full text-xs md:text-xl font-medium opacity-0 group-hover:opacity-100">Transformasi digital manajemen persuratan(mBerkas)</div>
       </div>
-
-      <div class="absolute top-0 -left-4 h-full items-center hidden md:flex">
-        <button role="button" class="prev2 px-2 py-2 rounded-full bg-abu text-black group" aria-label="prev"><svg
-            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="w-5 h-5 group-active:-translate-x-2 transition-all duration-200 ease-linear">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-          </svg>
-
-        </button>
+      <div class="b relative group">
+        <div class="slideim aspect-[4/3] h-full flex-shrink-0 snap-center rounded overflow-hidden transition-transform transform-gpu">
+          <img class="w-full h-full object-cover " src="./assets/galeri/foto2.jpeg" alt="Kesepakatan Kerja Sama">
+        </div>
+        <div class="absolute inset-0 flex items-center justify-center [text-shadow:0_1px_0_rgb(0_0_0/_100%)] w-full bg-black opacity-0 group-hover:opacity-50"></div>
+        <div class="absolute inset-0 flex items-center justify-center [text-shadow:0_1px_0_rgb(0_0_0/_100%)] text-white w-full text-xs md:text-xl font-medium opacity-0 group-hover:opacity-100">Kesepakatan Kerja Sama</div>
       </div>
-      <div class="absolute top-0 -right-4 h-full items-center hidden md:flex">
-        <button role="button" class="next2 px-2 py-2 rounded-full bg-abu text-black group" aria-label="next"><svg
-            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-            class="w-5 h-5 group-active:translate-x-2 transition-all duration-200 ease-linear">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-          </svg>
-        </button>
+      <div class="b relative group">
+        <div class="slideim aspect-[4/3] h-full flex-shrink-0 snap-center rounded overflow-hidden transition-transform transform-gpu">
+          <img class="w-full h-full object-cover " src="./assets/galeri/foto3.jpeg" alt="Pengabdian Masyarakat">
+        </div>
+        <div class="absolute inset-0 flex items-center justify-center [text-shadow:0_1px_0_rgb(0_0_0/_100%)] w-full bg-black opacity-0 group-hover:opacity-50"></div>
+        <div class="absolute inset-0 flex items-center justify-center [text-shadow:0_1px_0_rgb(0_0_0/_100%)] text-white w-full text-xs md:text-xl font-medium opacity-0 group-hover:opacity-100">Pengabdian Masyarakat</div>
+      </div>
+      <div class="b relative group">
+        <div class="slideim aspect-[4/3] h-full flex-shrink-0 snap-center rounded overflow-hidden transition-transform transform-gpu">
+          <img class="w-full h-full object-cover" src="./assets/galeri/foto4.jpeg" alt="Rapat Kerja Fakultas Teknik">
+        </div>
+        <div class="absolute inset-0 flex items-center justify-center [text-shadow:0_1px_0_rgb(0_0_0/_100%)] w-full bg-black opacity-0 group-hover:opacity-50"></div>
+        <div class="absolute inset-0 flex items-center justify-center [text-shadow:0_1px_0_rgb(0_0_0/_100%)] text-white w-full text-xs md:text-xl font-medium opacity-0 group-hover:opacity-100">Rapat Kerja Fakultas Teknik</div>
+      </div>
+      <div class="b relative group">
+        <div class="slideim aspect-[4/3] h-full flex-shrink-0 snap-center rounded overflow-hidden transition-transform transform-gpu">
+          <img class="w-full h-full object-cover" src="./assets/galeri/foto5.jpeg" alt="Pelantikan dan Pengambilan sumpah jabatan Pejabat">
+        </div>
+        <div class="absolute inset-0 flex items-center justify-center [text-shadow:0_1px_0_rgb(0_0_0/_100%)] w-full bg-black opacity-0 group-hover:opacity-50"></div>
+        <div class="absolute inset-0 flex items-center justify-center [text-shadow:0_1px_0_rgb(0_0_0/_100%)] text-white w-full text-xs md:text-xl font-medium opacity-0 group-hover:opacity-100">Pelantikan dan Pengambilan sumpah jabatan Pejabat</div>
+      </div>
+      <div class="b relative group">
+        <div class="slideim aspect-[4/3] h-full flex-shrink-0 snap-center rounded overflow-hidden transition-transform transform-gpu">
+          <img class="max-w-screen-2xl h-full object-cover" src="./assets/galeri/foto6.jpeg" alt="Wisuda Universitas Hasanuddin Periode IV tahap II">
+        </div>
+        <div class="absolute inset-0 flex items-center justify-center [text-shadow:0_1px_0_rgb(0_0_0/_100%)] w-full bg-black opacity-0 group-hover:opacity-50"></div>
+        <div class="absolute inset-0 flex items-center justify-center [text-shadow:0_1px_0_rgb(0_0_0/_100%)] text-white w-full text-xs md:text-xl font-medium opacity-0 group-hover:opacity-100">Wisuda Universitas Hasanuddin Periode IV tahap II</div>
+      </div>
+      <div class="b relative group">
+        <div class="slideim aspect-[4/3] h-full flex-shrink-0 snap-center rounded overflow-hidden transition-transform transform-gpu">
+          <img class="w-full h-full object-cover" src="./assets/galeri/foto7.jpeg" alt="Rapat Koordinasi Internal">
+        </div>
+        <div class="absolute inset-0 flex items-center justify-center [text-shadow:0_1px_0_rgb(0_0_0/_100%)] w-full bg-black opacity-0 group-hover:opacity-50"></div>
+        <div class="absolute inset-0 flex items-center justify-center [text-shadow:0_1px_0_rgb(0_0_0/_100%)] text-white w-full text-xs md:text-xl font-medium opacity-0 group-hover:opacity-100">Rapat Koordinasi Internal</div>
       </div>
     </div>
+
+    <div class="absolute top-0 -left-4 h-full items-center hidden lg:flex">
+      <button role="button" class="prev2 px-2 py-2 rounded-full bg-neutral-100 text-neutral-900 group" aria-label="prev"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 group-active:-translate-x-2 transition-all duration-200 ease-linear">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+        </svg>
+
+      </button>
+    </div>
+    <div class="absolute top-0 -right-4 h-full items-center hidden lg:flex">
+      <button role="button" class="next2 px-2 py-2 rounded-full bg-neutral-100 text-neutral-900 group" aria-label="next"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 group-active:translate-x-2 transition-all duration-200 ease-linear">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+        </svg>
+      </button>
+    </div>
   </div>
+</div>
 
   <div class="dharmawarnita mt-30 flex mb-30 bg-abu">
     <div class="foto w-2/5 h-1/4">
